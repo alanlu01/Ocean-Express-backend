e2f98d911c16124d4b3ed1f4681fa2da091476c0
olalbertlo <albert0915a@gmail.com>
2025-11-25 02:47:16 +0800

    feat: POST/auth/login and POST/auth/register are done according to document

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,12 +2,14 @@ use axum::Router;
 use mongodb::Database;
 
 // api routes all import here
-mod retaurants;
+mod auth;
 mod menu;
+mod retaurants;
 
 pub fn api_router(db: Database) -> Router{
     // merge all routes(an api is an endpoint) here
     Router::new()
     .nest("/restaurants", retaurants::home_page_router(db.clone()))
     .nest("/restaurants", menu::menu_router(db.clone()))
+    .nest("/auth", auth::auth_router(db.clone()))
 }
