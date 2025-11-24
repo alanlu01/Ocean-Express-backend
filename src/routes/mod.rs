27da47c6ae1ce206188a4f9ba8eafa10e6c464de
olalbertlo <albert0915a@gmail.com>
2025-11-24 21:25:32 +0800

    fix: GET/restaurants and GET/restaurants/{id}, return json style fix

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,12 +2,12 @@ use axum::Router;
 use mongodb::Database;
 
 // api routes all import here
-mod home_page;
+mod retaurants;
 mod menu;
 
 pub fn api_router(db: Database) -> Router{
     // merge all routes(an api is an endpoint) here
     Router::new()
-    .nest("/home_page", home_page::home_page_router(db.clone()))
+    .nest("/restaurants", retaurants::home_page_router(db.clone()))
     .nest("/shops", menu::menu_router(db))
 }
