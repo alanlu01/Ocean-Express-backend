9dfa147997d95f234e65bc1934955296133ffa7a
olalbertlo <albert0915a@gmail.com>
2025-11-24 16:18:55 +0800

    feat: cart api, return all dish in 'cart' collection

--- src/routes/home_page.rs
+++ src/routes/home_page.rs
@@ -3,7 +3,9 @@ use mongodb::{bson::doc, bson::Document, Database};
 use futures::stream::TryStreamExt;
 
 async fn get_all_shops(State(db): State<Database>) -> Result<Json<Vec<Document>>, (StatusCode, String)>{
-    let collections = db.list_collection_names().await.map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, format!("List collections error: {}", e)))?;
+    let collections = db.list_collection_names()
+        .await
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, format!("List collections error: {}", e)))?;
     println!("collections in DB: {:?}", collections);
 
     if collections.is_empty() {
@@ -14,13 +16,17 @@ async fn get_all_shops(State(db): State<Database>) -> Result<Json<Vec<Document>>
     let coll_name = if collections.iter().any(|c| c == "shops") {
         "shops"
     } else {
-        &collections[0]
+        // db : 0 cart, 1 menu, 2 shops
+        &collections[2]
     };
 
     // test print
     println!("using collection: {}", coll_name);
     let collection = db.collection::<Document>(coll_name);
-    let mut cursor = collection.find(doc! {}).await.map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, format!("Find error: {}", e)))?;
+    let mut cursor = collection
+        .find(doc! {})
+        .await
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, format!("home_page error: {}", e)))?;
     let mut results = Vec::new();
     while let Some(doc) = cursor.try_next().await.map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, format!("Cursor error: {}", e)))? {
         results.push(doc);
